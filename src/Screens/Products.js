245f70a6ed5e0d9ba7ad43fece4486efd3ad5038
Mujahid1717 <mujahidakhtar2003@gmail.com>
2023-02-03 10:59:14 +0500

    final gridView

--- src/Screens/Products.js
+++ src/Screens/Products.js
@@ -8,25 +8,31 @@ const Products= ({products}) => {
     return(
         <View style = {styles.container}>
            <Image style ={styles.imageStyle}  source = {require('../../assets/logo.png')}/>
-           <Text>{name}</Text>
-           <Text>{brand}</Text> 
-           <Text>{price}</Text>
+           <Text style= {styles.nameText}>{name}</Text>
+           <Text>Brand:{brand}</Text> 
+           <Text>Price:{price}</Text>
         </View>
     )
     
 }
 const styles = StyleSheet.create({
     container:{
-            width: "48%",
-            alignItems: 'center',
-            margin:"1%",
-            
+        width: "48%",
+        alignItems: 'center',
+        margin:"1%",
+        padding:15,
+        borderWidth:1
 
     },
     imageStyle :{
         height:150,
         width: 150
-    }
+    },
+    nameText:{
+        fontWeight:"bold",
+        textAlign:'center',
+        padding:10
+    },
 
 });
 export default Products;
